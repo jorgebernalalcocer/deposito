6c9690df47a80b555a52f91aa6366b57f8a4d1b7
jorgebernalalcocer <j.90.bernal@gmail.com>
2022-02-07 17:09:00 +0100

    Puesta en marcha

---
+++ DepositoCombustible/test/DepositoCombustibleTest.java
@@ -0,0 +1,63 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DepositoCombustibleTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDepositoCombustible() {
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDepositoNivel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDepositoMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstaVacio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstaLleno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConsumir() {
+		fail("Not yet implemented");
+	}
+
+}
